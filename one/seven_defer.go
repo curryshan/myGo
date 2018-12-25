90cdb9cc3d95a547c7b9145c9e6e18b85ec9634e
shanmenghao <shanmenghao@baidu.com>
2018-12-25 23:37:52 +0800

    add defer demo code

---
+++ one/seven_defer.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+//  go中抛出一个panic异常 然后在defer中通过 recover捕获这个异常
+func main() {
+
+	defer func() {
+        fmt.Println("c")
+        if err := recover(); err != nil {
+            fmt.Println(err)
+        }
+        fmt.Println("d")
+	}()
+
+	f()
+}
+
+func f () {
+    fmt.Println("a")
+    panic("this is error")
+    fmt.Println("b")
+    fmt.Println("f")
+}
+
+/*
+a
+c
+this is error
+d
+*/
